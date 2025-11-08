30d10297ff6be6e0bdaad9b41b34d723b6350b79
tverdui-ponos <zxcantoshka1488@gmail.com>
2025-11-08 21:33:38 +0300

    аллах

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from aiogram import Bot, Dispatcher, Types
-from aiogram.utils import executor
+from aiogram import F
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
-API_TOKEN = "TOKEN"
+from aiogram.enums import ParseMode
+
+from aiogram.client.default import DefaultBotProperties
+
+import asyncio
 
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
 
-@dp.message_handler(commands='start')
-async def start(message: types.Message):
-    await message.reply("Здарова педеки")
+def token_read():
+    with open("token.txt", "r") as file: return file.readline()
+
+API_TOKEN = token_read()
+
+
+
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message) -> None:
+    await message.answer("Здарова педеки")
+
+@dp.message(F.text, Command("where_is_Iliya"))
+async def where(message:Message) -> None:
+    await message.reply("В <strong> пизде </strong>")
+
+
+
+
+async def main() -> None:
+    bot = Bot(token=API_TOKEN, 
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
